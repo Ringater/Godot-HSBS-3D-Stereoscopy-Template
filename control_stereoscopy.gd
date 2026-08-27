e24091d78d44d0e37b394c1c0c6c99ea733b35cd
Ringater <67206789+Ringater@users.noreply.github.com>
2026-08-27 12:52:58 -0700

    Version 2
    added support for top-and-bottom 3d and cursor functionality

--- control_stereoscopy.gd
+++ control_stereoscopy.gd
@@ -3,6 +3,7 @@ extends Node
 @export var enableStereoscopy = true;
 @export var setIPD = 0.06;
 @export var swapEyes = false;
+@export var topAndBottom = true;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,20 +16,38 @@ func _process(delta: float) -> void:
 	#garbage collection
 	for idx in $Control/SubViewportContainer2/SubViewport.get_children():
 		idx.free();
+	for idx in $Control/SubViewportContainer3/SubViewport.get_children():
+		idx.free();
 	
 	if enableStereoscopy:
 		
-		var Node3D2 = $Control/SubViewportContainer/SubViewport/Node3D.duplicate();
-		$Control/SubViewportContainer2/SubViewport.add_child(Node3D2);
-		
-		if swapEyes:
-			$Control/SubViewportContainer/SubViewport/Node3D/Camera3D.h_offset = setIPD;
-			$Control/SubViewportContainer2/SubViewport/Node3D/Camera3D.h_offset = 0-setIPD;
+		if topAndBottom:
+			
+			var Node3D2 = $Control/SubViewportContainer/SubViewport/Node3D.duplicate();
+			$Control/SubViewportContainer3/SubViewport.add_child(Node3D2);
+			
+			if swapEyes:
+				$Control/SubViewportContainer/SubViewport/Node3D/Camera3D.h_offset = setIPD;
+				$Control/SubViewportContainer3/SubViewport/Node3D/Camera3D.h_offset = 0-setIPD;
+			else:
+				$Control/SubViewportContainer/SubViewport/Node3D/Camera3D.h_offset = 0-setIPD;
+				$Control/SubViewportContainer3/SubViewport/Node3D/Camera3D.h_offset = setIPD;
+				
+			self.scale = Vector2(1, 0.5);
+			
 		else:
-			$Control/SubViewportContainer/SubViewport/Node3D/Camera3D.h_offset = 0-setIPD;
-			$Control/SubViewportContainer2/SubViewport/Node3D/Camera3D.h_offset = setIPD;
-		
-		self.scale = Vector2(0.5, 1);
+			
+			var Node3D2 = $Control/SubViewportContainer/SubViewport/Node3D.duplicate();
+			$Control/SubViewportContainer2/SubViewport.add_child(Node3D2);
+			
+			if swapEyes:
+				$Control/SubViewportContainer/SubViewport/Node3D/Camera3D.h_offset = setIPD;
+				$Control/SubViewportContainer2/SubViewport/Node3D/Camera3D.h_offset = 0-setIPD;
+			else:
+				$Control/SubViewportContainer/SubViewport/Node3D/Camera3D.h_offset = 0-setIPD;
+				$Control/SubViewportContainer2/SubViewport/Node3D/Camera3D.h_offset = setIPD;
+			
+			self.scale = Vector2(0.5, 1);
 		
 	else:
 		
